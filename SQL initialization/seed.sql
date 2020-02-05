e2dbe1e7040ed09849491ba80dea8f81f6d9e5cf
Andrew Bergheim <andrewbergheim@gmail.com>
2020-02-05 14:39:11 -0700

    sql init files working properly

---
+++ SQL initialization/seed.sql
@@ -0,0 +1,10 @@
+use company_db;
+insert into department (id,name) VALUES (1, 'Peacekeepers');
+insert into department (id,name) VALUES (2, 'Sales');
+
+insert into roles (id, title, salary, department_id) VALUES (1, 'Mall Cop', 42000.00, 1);
+insert into roles (id, title, salary, department_id) VALUES (2, 'Propane',57000.00, 2);
+
+
+insert into employees (id, first_name, last_name, role_id) VALUES (1, 'Paul', 'Blart', 1);
+insert into employees (id, first_name, last_name, role_id) VALUES (2, 'Hank', 'Hill', 2);
